886588e4cab2ca0c70db9528c3877d6dfa0b8c70
Shakir Shabbir <shakirshabbir@pc-133-25.customer.ask4.lan>
2023-02-07 00:51:18 +0000

    Initial commit for face recognition project

---
+++ Face_Detector.py
@@ -0,0 +1,20 @@
+import cv2
+from matplotlib import pyplot as plt
+
+#Import pretrained dataset
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#Import the Image to Detect face(s) in
+img = cv2.imread('IMG_0452.jpg')
+
+#covert to gray scale
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#Detect Face(s)
+face_coordinate = trained_face_data.detectMultiScale(grayscaled_img)
+print(face_coordinate)
+(x,y,w,h) = face_coordinate[0]
+cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow('Clever face detector', img)
+cv2.waitKey()
